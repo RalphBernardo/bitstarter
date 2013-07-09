dd59fa36c59a1d93667ba29dde14eb2dbd604cab
Ralph Bernardo <ralphbernardo@gmail.com>
2013-07-09 17:18:00 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
   var fs = require('fs');
-  var buffer = new buffer ();
+  var buffer = new Buffer(256);
   response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
 });
 
