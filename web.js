71c3c4b818a609e2cd884b5b21cae705d917e9b8
Ralph Bernardo <ralphbernardo@gmail.com>
2013-07-09 17:27:27 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync("index.html");
+var buffer = new Buffer(index);
+
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
-  var fs = require('fs');
-  var buffer = new Buffer(256);
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
